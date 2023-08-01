c744689b073d568f0682743275163100306d92b4
tangzimin <875961336@qq.com>
2023-08-01 09:30:55 +0800

    多一行， tzm

--- Scripts/Login Test/Script1690549179424.groovy
+++ Scripts/Login Test/Script1690549179424.groovy
@@ -32,5 +32,5 @@ WebUI.verifyElementText(findTestObject('Object Repository/Login Page/Page_TestPr
 
 WebUI.closeBrowser()
 
-WebUI.comment('Emma added this comment')
+WebUI.comment('tzm added this comment')
 
